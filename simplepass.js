f27310efed5e38a1ff6c891403dc5db7a0b38871
7ABehrendt <7abehrendt@stpatrick.org>
2017-04-26 11:25:11 -0500

    Add files via upload

---
+++ simplepass.js
@@ -0,0 +1,15 @@
+//type checkString below
+function checkString(str){
+    if(str.length<7 && str.length>19){
+      throw RangeError("It is not between 8-20. Dumbass.");
+    }
+    else if(){
+      return true
+    }
+}
+
+
+//type getString below
+function getString(str){
+  
+}
